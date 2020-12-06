a5bbf1d0fde5dde7ba8ab63e34da986348fd5d7e
Paul Grandperrin <paul.grandperrin@gmail.com>
2020-12-06 14:47:25 -0300

    change wasm logger

--- client-web/src/bin/app.rs
+++ client-web/src/bin/app.rs
@@ -1,6 +1,17 @@
 use client_web::Question;
 
+cfg_if::cfg_if! {
+    if #[cfg(feature = "console_log")] {
+        fn init_log() {
+            use log::Level;
+            console_log::init_with_level(Level::Trace).expect("error initializing log");
+        }
+    } else {
+        fn init_log() {}
+    }
+}
+
 fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
+    init_log();
     yew::start_app::<client_web::Question>();
 }
