fc124ed01c737aba1adb958106b7a41f404a2b08
Paul Grandperrin <paul.grandperrin@gmail.com>
2020-12-13 17:03:28 -0300

    fix opaque pk

--- server/src/state.rs
+++ server/src/state.rs
@@ -1,14 +1,16 @@
+use std::sync::Arc;
+use opaque_ke::ciphersuite::CipherSuite;
 
 #[derive(Debug, Clone)]
 pub struct State {
-    pub opaque_pk: <common::crypto::Default as opaque_ke::ciphersuite::CipherSuite>::KeyFormat
+    pub opaque_pk: Arc<<common::crypto::Default as CipherSuite>::KeyFormat>
 }
 
 impl State {
     pub fn new() -> Self {
         let mut rng = rand_core::OsRng;
         Self {
-            opaque_pk: <common::crypto::Default as opaque_ke::ciphersuite::CipherSuite>::generate_random_keypair(&mut rng).unwrap()
+            opaque_pk: Arc::new(<common::crypto::Default as CipherSuite>::generate_random_keypair(&mut rng).unwrap())
         }
     }
 }
