044134b4f4bb163c83e63750d4d6c64fa788b10a
Paul Grandperrin <paul.grandperrin@gmail.com>
2020-12-29 17:03:28 -0300

    fix

--- server/src/state.rs
+++ server/src/state.rs
@@ -5,7 +5,7 @@ use crate::db::sql::Db;
 
 #[derive(Debug, Clone)]
 pub struct State {
-    pub opaque_pk: Arc<<common::crypto::Default as CipherSuite>::KeyFormat>,
+    pub opaque_pk: Arc<<common::crypto::OpaqueConf as CipherSuite>::KeyFormat>,
     pub db: Db,
 }
 
@@ -13,7 +13,7 @@ impl State {
     pub async fn new() -> anyhow::Result<Self> {
         let mut rng = rand_core::OsRng;
         Ok(Self {
-            opaque_pk: Arc::new(<common::crypto::Default as CipherSuite>::generate_random_keypair(&mut rng).unwrap()),
+            opaque_pk: Arc::new(<common::crypto::OpaqueConf as CipherSuite>::generate_random_keypair(&mut rng).unwrap()),
             db: Db::new().await?
         })
     }
