7412621d61ca10ed5e71c611d1f765944141549e
Paul Grandperrin <paul.grandperrin@gmail.com>
2020-12-31 15:44:57 -0300

    simplify server RPC design

--- server/src/core/auth.rs
+++ server/src/core/auth.rs
@@ -2,102 +2,92 @@ use std::convert::TryFrom;
 
 use anyhow::anyhow;
 use chrono::Duration;
-use common::api::{self, GetUserIdFromEmail, LoginFinish, LoginStart, SignupFinish, SignupStart};
+use common::api::{self, GetUserIdFromEmail, LoginFinish, LoginStart, Rpc, SignupFinish, SignupStart};
 use opaque_ke::{CredentialFinalization, CredentialRequest, RegistrationRequest, RegistrationUpload, ServerLogin, ServerLoginStartParameters, ServerRegistration, keypair::KeyPair};
 use rand::Rng;
+use serde::Serialize;
 use tide::Request;
 use tracing::{error, info, trace};
 use common::crypto::OpaqueConf;
 use async_trait::async_trait;
 
-use crate::rpc::{RpcExec, RpcRetEncode};
 
-#[async_trait]
-impl RpcExec for GetUserIdFromEmail {
-    async fn exec(&self, req: Request<crate::state::State>) -> anyhow::Result<Box<dyn RpcRetEncode>> {
-        let user_id = req.state().db.get_user_id_from_email(&self.email).await?;
+pub async fn get_user_id_from_email(req: Request<crate::state::State>, args: GetUserIdFromEmail) -> anyhow::Result<<GetUserIdFromEmail as Rpc>::Ret> {
+    let user_id = req.state().db.get_user_id_from_email(&args.email).await?;
 
-        Ok(Box::new(user_id))
-    }
+    Ok(user_id)
 }
 
 
-#[async_trait]
-impl RpcExec for SignupStart {
-    async fn exec(&self, req: Request<crate::state::State>) -> anyhow::Result<Box<dyn RpcRetEncode>> {
-        let mut rng = rand_core::OsRng;
-        let opaque = ServerRegistration::<OpaqueConf>::start(
-            &mut rng,
-            RegistrationRequest::deserialize(&self.opaque_msg)?,
-            req.state().opaque_kp.public(),
-        )?;
-        let opaque_state = opaque.state.to_bytes();
+pub async fn signup_start(req: Request<crate::state::State>, args: SignupStart) -> anyhow::Result<<SignupStart as Rpc>::Ret> {
+    let mut rng = rand_core::OsRng;
+    let opaque = ServerRegistration::<OpaqueConf>::start(
+        &mut rng,
+        RegistrationRequest::deserialize(&args.opaque_msg)?,
+        req.state().opaque_kp.public(),
+    )?;
+    let opaque_state = opaque.state.to_bytes();
 
-        let user_id: [u8; 32] = rand::thread_rng().gen(); // 256bits, so I don't even have to think about birthday attacks
-        let ip = req.peer_addr().map(|a|{a.split(':').next()}).flatten().ok_or_else(||{anyhow!("failed to determine client ip")})?;
-        let expiration = (chrono::Utc::now() + Duration::minutes(1)).timestamp();
-        
-        req.state().db.save_opaque_state(&user_id, ip, expiration, &opaque_state).await?;
+    let user_id: [u8; 32] = rand::thread_rng().gen(); // 256bits, so I don't even have to think about birthday attacks
+    let ip = req.peer_addr().map(|a|{a.split(':').next()}).flatten().ok_or_else(||{anyhow!("failed to determine client ip")})?;
+    let expiration = (chrono::Utc::now() + Duration::minutes(1)).timestamp();
+    
+    req.state().db.save_opaque_state(&user_id, ip, expiration, &opaque_state).await?;
 
-        Ok(Box::new((
-            user_id.to_vec(),
-            opaque.message.serialize()
-        )))
-    }
+    Ok((
+        user_id.to_vec(),
+        opaque.message.serialize()
+    ))
 }
 
-#[async_trait]
-impl RpcExec for SignupFinish {
-    async fn exec(&self, req: Request<crate::state::State>) -> anyhow::Result<Box<dyn RpcRetEncode>> {
-        let opaque_state = req.state().db.restore_opaque_state(&self.user_id).await?;
-        let opaque_state = ServerRegistration::<OpaqueConf>::try_from(&opaque_state[..])?;
-    
-        let opaque_password = opaque_state
-            .finish(RegistrationUpload::deserialize(&self.opaque_msg)?)?;
-    
-        req.state().db.insert_user(&self.user_id, &self.email, &opaque_password.to_bytes()).await?;
-    
-        Ok(Box::new(()))
-    }
+
+pub async fn signup_finish(req: Request<crate::state::State>, args: SignupFinish) -> anyhow::Result<<SignupFinish as Rpc>::Ret> {
+    let opaque_state = req.state().db.restore_opaque_state(&args.user_id).await?;
+    let opaque_state = ServerRegistration::<OpaqueConf>::try_from(&opaque_state[..])?;
+
+    let opaque_password = opaque_state
+        .finish(RegistrationUpload::deserialize(&args.opaque_msg)?)?;
+
+    req.state().db.insert_user(&args.user_id, &args.email, &opaque_password.to_bytes()).await?;
+
+    Ok(())
 }
 
-#[async_trait]
-impl RpcExec for LoginStart {
-    async fn exec(&self, req: Request<crate::state::State>) -> anyhow::Result<Box<dyn RpcRetEncode>> {
-        let mut rng = rand_core::OsRng;
 
-        let opaque_password = req.state().db.get_opaque_password_from_user_id(&self.user_id).await?;
-        
-        let opaque_password = ServerRegistration::<OpaqueConf>::try_from(&opaque_password[..])?;
-        let opaque = ServerLogin::start(
-            &mut rng,
-            opaque_password,
-            req.state().opaque_kp.private(),
-            CredentialRequest::deserialize(&self.opaque_msg)?,
-            ServerLoginStartParameters::default(), // FIXME 
-        )?;
+
+pub async fn login_start(req: Request<crate::state::State>, args: LoginStart) -> anyhow::Result<<LoginStart as Rpc>::Ret> {
+    let mut rng = rand_core::OsRng;
+
+    let opaque_password = req.state().db.get_opaque_password_from_user_id(&args.user_id).await?;
     
-        let ip = req.peer_addr().map(|a|{a.split(':').next()}).flatten().ok_or_else(||{anyhow!("failed to determine client ip")})?;
-        let expiration = (chrono::Utc::now() + Duration::minutes(1)).timestamp();
-        req.state().db.save_opaque_state(&self.user_id, ip, expiration, &opaque.state.to_bytes()).await?;
-        
-        let opaque_msg = opaque.message.serialize();
+    let opaque_password = ServerRegistration::<OpaqueConf>::try_from(&opaque_password[..])?;
+    let opaque = ServerLogin::start(
+        &mut rng,
+        opaque_password,
+        req.state().opaque_kp.private(),
+        CredentialRequest::deserialize(&args.opaque_msg)?,
+        ServerLoginStartParameters::default(), // FIXME 
+    )?;
+
+    let ip = req.peer_addr().map(|a|{a.split(':').next()}).flatten().ok_or_else(||{anyhow!("failed to determine client ip")})?;
+    let expiration = (chrono::Utc::now() + Duration::minutes(1)).timestamp();
+    req.state().db.save_opaque_state(&args.user_id, ip, expiration, &opaque.state.to_bytes()).await?;
     
-        Ok(Box::new(opaque_msg))
-    }
+    let opaque_msg = opaque.message.serialize();
+
+    Ok(opaque_msg)
 }
 
-#[async_trait]
-impl RpcExec for LoginFinish {
-    async fn exec(&self, req: Request<crate::state::State>) -> anyhow::Result<Box<dyn RpcRetEncode>> {
-        let opaque_state = req.state().db.restore_opaque_state(&self.user_id).await?;
-        let opaque_state = ServerLogin::<OpaqueConf>::try_from(&opaque_state[..])?;
-        let _opaque_log_finish_result =opaque_state.finish(CredentialFinalization::deserialize(&self.opaque_msg)?)?;
-    
-        //opaque_log_finish_result.shared_secret
-    
-        Ok(Box::new(()))
-    }
+
+
+pub async fn login_finish(req: Request<crate::state::State>, args: LoginFinish) -> anyhow::Result<<LoginFinish as Rpc>::Ret> {
+    let opaque_state = req.state().db.restore_opaque_state(&args.user_id).await?;
+    let opaque_state = ServerLogin::<OpaqueConf>::try_from(&opaque_state[..])?;
+    let _opaque_log_finish_result =opaque_state.finish(CredentialFinalization::deserialize(&args.opaque_msg)?)?;
+
+    //opaque_log_finish_result.shared_secret
+
+    Ok(())
 }
 
 
--- server/src/rpc.rs
+++ server/src/rpc.rs
@@ -9,38 +9,17 @@ use async_trait::async_trait;
 
 use crate::core::auth;
 
-pub trait RpcRetEncode {
-    fn encode(&self) -> anyhow::Result<Vec<u8>>;
-}
-
-impl<T: Serialize> RpcRetEncode for T {
-    fn encode(&self) -> anyhow::Result<Vec<u8>> {
-        Ok(rmp_serde::to_vec_named(&self)?)
-    }
-}
-
-#[async_trait]
-pub trait RpcExec: Send + Sync {
-    async fn exec(&self, req: Request<crate::state::State>) -> anyhow::Result<Box<dyn RpcRetEncode>>;
-}
-
-async fn exec(req: Request<crate::state::State>, c: Call) -> anyhow::Result<Vec<u8>> {
-    let c: Box<dyn RpcExec> = match c {
-        Call::GetUserIdFromEmail(i) => Box::new(i),
-        Call::SignupStart(i) => Box::new(i),
-        Call::SignupFinish(i) => Box::new(i),
-        Call::LoginStart(i) => Box::new(i),
-        Call::LoginFinish(i) => Box::new(i),
-    };
-
-    c.exec(req).await?.encode()
-}
-
-
 pub async fn rpc(mut req: Request<crate::state::State>) -> tide::Result {
     let body = req.body_bytes().await?;
     let c: api::Call = rmp_serde::from_slice(&body)?;
 
-    let resp = exec(req, c).await?;
-    Ok(Body::from_bytes(resp).into())
+    let resp = match c {
+        Call::GetUserIdFromEmail(args) => rmp_serde::encode::to_vec_named(&auth::get_user_id_from_email(req, args).await?),
+        Call::SignupStart(args) => rmp_serde::encode::to_vec_named(&auth::signup_start(req, args).await?),
+        Call::SignupFinish(args) => rmp_serde::encode::to_vec_named(&auth::signup_finish(req, args).await?),
+        Call::LoginStart(args) => rmp_serde::encode::to_vec_named(&auth::login_start(req, args).await?),
+        Call::LoginFinish(args) => rmp_serde::encode::to_vec_named(&auth::login_finish(req, args).await?),
+    };
+
+    Ok(Body::from_bytes(resp?).into())
 }
