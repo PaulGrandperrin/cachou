6b1ddf3583c034e1761b2807c46b32ac066399c7
Paul Grandperrin <paul.grandperrin@gmail.com>
2021-02-03 21:52:55 +0100

    update dependancies

--- common/src/crypto/opaque.rs
+++ common/src/crypto/opaque.rs
@@ -30,7 +30,7 @@ impl<D: opaque_ke::hash::Hash> SlowHash<D> for SlowHashArgon {
 
 pub struct OpaqueConf;
 impl CipherSuite for OpaqueConf {
-    type Group = curve25519_dalek::ristretto::RistrettoPoint;
+    type Group = curve25519_dalek_ng::ristretto::RistrettoPoint;
     type KeyFormat = opaque_ke::keypair::X25519KeyPair;
     type KeyExchange = opaque_ke::key_exchange::tripledh::TripleDH;
     type Hash = sha2::Sha256;
