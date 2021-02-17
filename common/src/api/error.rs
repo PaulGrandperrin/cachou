9b32e917cb30f4beb85431458a6538ec7b567bc2
Paul Grandperrin <paul.grandperrin@gmail.com>
2021-02-17 17:06:59 +0100

    WIP: use api::Error in sql.rs

--- common/src/api/error.rs
+++ common/src/api/error.rs
@@ -3,10 +3,20 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Error, Debug, Serialize, Deserialize)]
 pub enum Error {
+    /* expected and normal business logic related errors that must be handled by the client */
+
     #[error("Invalid session token")]
     InvalidSessionToken,
     #[error("Username conflict")]
     UsernameConflict,
+    #[error("Username not found")]
+    UsernameNotFound,
+
+    /* Execution errors which interrupted request processing but falls outside normal operation.
+       Intentionnaly doesn't specify if expected or not, nor if client-side or server-side
+       as to not leak potential information. Also hides the actual error at serialization.
+       Client is just expected to report the error to the user as a server-related error.
+       Similar to http 500 code. */
     #[error(transparent)]
     ExecutionError(#[from] #[serde(skip, default = "default_error")] eyre::Report), // FIXME at serialization, encrypt the error or replace it with its location in the logs
 }
--- server/src/db/sql.rs
+++ server/src/db/sql.rs
@@ -1,6 +1,7 @@
 use std::{time::Duration};
 
-use sqlx::{Executor, Row, mysql::{MySqlConnectOptions, MySqlPoolOptions, MySqlRow}};
+use common::api;
+use sqlx::{Executor, Row, mysql::{MySqlConnectOptions, MySqlDatabaseError, MySqlPoolOptions, MySqlRow}};
 use tracing::{error, trace};
 
 // making things generic doesn't seem very useful at this point
@@ -92,74 +93,84 @@ impl Db {
     }
 
     #[tracing::instrument]
-    pub async fn save_tmp(&self, session_id: &[u8], ip: &str, expiration: i64, field: &str, data: &[u8]) -> eyre::Result<()> {
+    pub async fn save_tmp(&self, session_id: &[u8], ip: &str, expiration: i64, field: &str, data: &[u8]) -> api::Result<()> {
         sqlx::query("replace into `tmp` values (?, INET_ATON(?), FROM_UNIXTIME(?), ?, ?)")
         .bind(session_id)
         .bind(ip)
         .bind(expiration)
         .bind(field)
         .bind(data)
-        .execute(&self.pool).await?;
+        .execute(&self.pool).await.map_err(|e| eyre::eyre!(e))?;
         Ok(())
     }
 
     #[tracing::instrument]
-    pub async fn restore_tmp(&self, session_id: &[u8], field: &str) -> eyre::Result<Vec<u8>> {
-        let mut tx = self.pool.begin().await?;
+    pub async fn restore_tmp(&self, session_id: &[u8], field: &str) -> api::Result<Vec<u8>> {
+        let mut tx = self.pool.begin().await.map_err(|e| eyre::eyre!(e))?;
         
         let row: MySqlRow = sqlx::query("select `data` from `tmp` where `session_id` = ? and `field` = ?")
         .bind(session_id)
         .bind(field)
-        .fetch_one(&mut tx).await?;
-        let state: Vec<u8> = row.try_get(0)?;
+        .fetch_one(&mut tx).await.map_err(|e| eyre::eyre!(e))?;
+        let state: Vec<u8> = row.try_get(0).map_err(|e| eyre::eyre!(e))?;
 
         sqlx::query("delete from `tmp` where `session_id` = ? and `field` = ?")
         .bind(session_id)
         .bind(field)
-        .execute(&mut tx).await?;
+        .execute(&mut tx).await.map_err(|e| eyre::eyre!(e))?;
 
-        tx.commit().await?;
+        tx.commit().await.map_err(|e| eyre::eyre!(e))?;
 
         Ok(state)
     }
 
     #[tracing::instrument]
-    pub async fn insert_user(&self, user_id: &[u8], username: &str, opaque_password: &[u8], hashed_secret_id: &[u8], sealed_masterkey: &[u8], sealed_private_data: &[u8]) -> eyre::Result<()> {
+    pub async fn insert_user(&self, user_id: &[u8], username: &str, opaque_password: &[u8], hashed_secret_id: &[u8], sealed_masterkey: &[u8], sealed_private_data: &[u8]) -> api::Result<()> {
         sqlx::query("insert into `user` values (?, ?, ?, ?, ?, ?)")
-        .bind(user_id)
-        .bind(username)
-        .bind(opaque_password)
-        .bind(hashed_secret_id)
-        .bind(sealed_masterkey)
-        .bind(sealed_private_data)
-        .execute(&self.pool).await?;
+                .bind(user_id)
+                .bind(username)
+                .bind(opaque_password)
+                .bind(hashed_secret_id)
+                .bind(sealed_masterkey)
+                .bind(sealed_private_data)
+                .execute(&self.pool).await.map_err(|e| {
+                    match e {
+                        sqlx::Error::Database(e)
+                            if e.as_error().downcast_ref::<MySqlDatabaseError>().map(|e| e.number()) == Some(1062)
+                            => api::Error::UsernameConflict,
+                        _ => api::Error::ExecutionError(e.into()),
+                    }
+                })?;
+
         Ok(())
     }
 
     #[tracing::instrument]
-    pub async fn get_userid_and_opaque_password_from_username(&self, username: &str) -> eyre::Result<(Vec<u8>, Vec<u8>)> {
+    pub async fn get_userid_and_opaque_password_from_username(&self, username: &str) -> api::Result<(Vec<u8>, Vec<u8>)> {
         let row: MySqlRow = sqlx::query("select `user_id`, `opaque_password` from `user` where `username` = ?")
         .bind(username)
-        .fetch_one(&self.pool).await?;
+        .fetch_one(&self.pool).await.map_err(|e| {
+            match e {
+                sqlx::Error::RowNotFound => api::Error::UsernameNotFound,
+                _ => api::Error::ExecutionError(e.into()),
+            }
+        })?;
 
-        Ok((row.try_get(0)?, row.try_get(1)?))
+        Ok((
+            row.try_get(0).map_err(|e| eyre::eyre!(e))?,
+            row.try_get(1).map_err(|e| eyre::eyre!(e))?,
+        ))
     }
 
     #[tracing::instrument]
-    pub async fn get_user_id_from_email(&self, email: &str) -> eyre::Result<Vec<u8>> {
-        let row: MySqlRow = sqlx::query("select `user_id` from `user` where `email` = ?")
-        .bind(email)
-        .fetch_one(&self.pool).await?;
-
-        Ok(row.try_get(0)?)
-    }
-
-    #[tracing::instrument]
-    pub async fn get_user_data_from_userid(&self, user_id: &[u8]) -> eyre::Result<(Vec<u8>, Vec<u8>)> {
+    pub async fn get_user_data_from_userid(&self, user_id: &[u8]) -> api::Result<(Vec<u8>, Vec<u8>)> {
         let row: MySqlRow = sqlx::query("select `sealed_masterkey`, `sealed_private_data` from `user` where `user_id` = ?")
         .bind(user_id)
-        .fetch_one(&self.pool).await?;
+        .fetch_one(&self.pool).await.map_err(|e| eyre::eyre!(e))?; // do not leak in returned error if the user_id exists or not
 
-        Ok((row.try_get(0)?, row.try_get(1)?))
+        Ok((
+            row.try_get(0).map_err(|e| eyre::eyre!(e))?,
+            row.try_get(1).map_err(|e| eyre::eyre!(e))?,
+        ))
     }
 }
