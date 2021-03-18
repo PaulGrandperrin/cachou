7cf4692fb0a84b608fcf252c3cfcf713ba17ad76
Paul Grandperrin <paul.grandperrin@gmail.com>
2021-03-18 12:50:11 +0100

    finish implementing master_key rotation and token revokation

--- server/src/core/auth.rs
+++ server/src/core/auth.rs
@@ -1,7 +1,7 @@
 use common::{api::{self, AddUser, AddUserRet, Credentials, ExportKey, GetExportKeys, GetExportKeysRet, GetUserPrivateData, GetUserPrivateDataRet, LoginFinish, LoginFinishRet, LoginStart, LoginStartRet, MasterKey, NewCredentials, NewCredentialsRet, OpaqueState, RotateMasterKey, RotateMasterKeyRet, RpcTrait, SecretServerState, SetCredentials, SetUserPrivateData, UserId, session_token::{Clearance, SessionToken}}, consts::{OPAQUE_S_ID, OPAQUE_S_ID_RECOVERY}, crypto::crypto_boxes::{AeadBox, SecretBox}};
 use tracing::{Instrument, debug, info, info_span};
 
-use crate::{db::DbConn, opaque, state::State};
+use crate::{db::{DbConn, sql::TxConn}, opaque, state::State};
 use crate::db::sql::Queryable;
 use serde::{Serialize, Deserialize};
 
@@ -20,14 +20,16 @@ struct ServerLoginState {
 
 impl State {
     pub async fn add_user(&self, args: &AddUser, conn: &mut DbConn<'_>) -> api::Result<<AddUser as RpcTrait>::Ret> {
-        let user_id = UserId::gen(); // 128bits, so I don't even have to think about birthday attacks
+        let user_id = UserId::gen();
         
         async {
             // start transaction
             let tx = conn.tx().await?;
 
+            let version_master_key = 0;
+
             // save user
-            tx.new_user(&user_id, 0).await?;
+            tx.new_user(&user_id, version_master_key).await?;
             
             // save normal and recovery credentials
             self.set_credentials_impl(tx, true, &args.credentials, false, &user_id).await?;
@@ -36,7 +38,7 @@ impl State {
             // save private data
             tx.set_user_private_data(&user_id, &args.secret_private_data).await?;
 
-            let authed_session_token = self.session_token_new_logged_in_sealed(user_id.clone(), 0, false, true)?;
+            let authed_session_token = self.session_token_new_logged_in_sealed(user_id.clone(), version_master_key, false, true)?;
 
             info!("ok");
             
@@ -57,7 +59,7 @@ impl State {
         })
     }
 
-    async fn set_credentials_impl(&self, conn: &mut impl Queryable, new: bool, credentials: &Credentials, recovery: bool, user_id: &UserId) -> api::Result<()> {
+    async fn set_credentials_impl(&self, conn: &mut TxConn, new: bool, credentials: &Credentials, recovery: bool, user_id: &UserId) -> api::Result<()> {
         let ServerCredentialsState { opaque_state } = AeadBox::<ServerCredentialsState, ()>::unseal(&self.secret_key, credentials.secret_server_state.as_slice())?.0;
         let opaque_password = opaque::registration_finish(&opaque_state, &credentials.opaque_msg)?;
 
@@ -71,11 +73,11 @@ impl State {
 
     pub async fn set_credentials(&self, args: &SetCredentials, conn: &mut DbConn<'_>) -> api::Result<<SetCredentials as RpcTrait>::Ret> {
         // get user's user_id and check that token has uber rights
-        let session_token = self.session_token_unseal_refreshed_and_validated(&args.authed_session_token, Clearance::Uber).await?;
+        let session_token = self.session_token_unseal_refreshed_and_validated(conn.tx().await?, &args.authed_session_token, Clearance::Uber).await?;
         let user_id = bs58::encode(session_token.user_id.as_slice()).into_string();
 
         async {
-            self.set_credentials_impl(conn.std().await?, false, &args.credentials, args.recovery, &session_token.user_id).await?;
+            self.set_credentials_impl(conn.tx().await?, false, &args.credentials, args.recovery, &session_token.user_id).await?;
 
             debug!("ok");
             
@@ -85,11 +87,11 @@ impl State {
 
     pub async fn get_export_keys(&self, args: &GetExportKeys, conn: &mut DbConn<'_>) -> api::Result<<GetExportKeys as RpcTrait>::Ret> {
         // get user's user_id and check that token has uber rights
-        let session_token = self.session_token_unseal_refreshed_and_validated(&args.authed_session_token, Clearance::Uber).await?;
+        let session_token = self.session_token_unseal_refreshed_and_validated(conn.tx().await?, &args.authed_session_token, Clearance::Uber).await?;
         let user_id = bs58::encode(session_token.user_id.as_slice()).into_string();
 
         async {
-            let (secret_export_key, secret_export_key_recovery) = conn.std().await?.get_export_keys(&session_token.user_id).await?;
+            let (secret_export_key, secret_export_key_recovery) = conn.tx().await?.get_export_keys(&session_token.user_id).await?;
 
             debug!("ok");
             
@@ -102,13 +104,13 @@ impl State {
 
     pub async fn rotate_master_key(&self, args: &RotateMasterKey, conn: &mut DbConn<'_>) -> api::Result<<RotateMasterKey as RpcTrait>::Ret> {
         // get user's user_id and check that token has uber rights
-        let session_token = self.session_token_unseal_refreshed_and_validated(&args.authed_session_token, Clearance::Uber).await?;
+        let session_token = self.session_token_unseal_refreshed_and_validated(conn.tx().await?, &args.authed_session_token, Clearance::Uber).await?;
         let user_id = bs58::encode(session_token.user_id.as_slice()).into_string();
 
         async {
             conn.tx().await?.rotate_master_key(
                 &session_token.user_id,
-                0,
+                session_token.version_master_key + 1,
                 &args.secret_private_data,
                 &args.secret_master_key,
                 &args.secret_export_key,
@@ -121,12 +123,13 @@ impl State {
     }
 
     pub async fn login_start(&self, args: &LoginStart, conn: &mut DbConn<'_>) -> api::Result<<LoginStart as RpcTrait>::Ret> {
-        let (user_id, opaque_password, secret_master_key) = conn.std().await?.get_credentials_from_username(args.recovery, &args.username).await?;
+        let (user_id, opaque_password, secret_master_key) = conn.tx().await?.get_credentials_from_username(args.recovery, &args.username).await?;
 
         async {
             // TODO if recovery, alert user (by mail) and block request for a few days
+            let version_master_key = conn.tx().await?.get_user_version_master_key(&user_id).await?;
             let (opaque_state, opaque_msg) = opaque::login_start(self.opaque_kp.private(), &args.opaque_msg, &args.username, &opaque_password, if args.recovery { &OPAQUE_S_ID_RECOVERY } else { &OPAQUE_S_ID })?;
-            let secret_server_state: SecretServerState = AeadBox::seal(&self.secret_key[..], &ServerLoginState{opaque_state, user_id: user_id.clone(), secret_master_key, version_master_key: 0}, &())?.into(); // TODO add TTL
+            let secret_server_state: SecretServerState = AeadBox::seal(&self.secret_key[..], &ServerLoginState{opaque_state, user_id: user_id.clone(), secret_master_key, version_master_key}, &())?.into(); // TODO add TTL
 
             info!("ok");
             Ok(LoginStartRet {
@@ -164,10 +167,10 @@ impl State {
     }
 
     pub async fn get_user_private_data(&self, args: &GetUserPrivateData, conn: &mut DbConn<'_>) -> api::Result<<GetUserPrivateData as RpcTrait>::Ret> {
-        let SessionToken{user_id, version_master_key: version, ..} = self.session_token_unseal_refreshed_and_validated(&args.authed_session_token, Clearance::LoggedIn).await?;
+        let SessionToken{user_id, ..} = self.session_token_unseal_refreshed_and_validated(conn.tx().await?, &args.authed_session_token, Clearance::LoggedIn).await?;
 
         async {
-            let secret_private_data = conn.std().await?.get_user_private_data(&user_id).await?;
+            let secret_private_data = conn.tx().await?.get_user_private_data(&user_id).await?;
 
             debug!("ok");
             Ok( GetUserPrivateDataRet {
@@ -177,10 +180,10 @@ impl State {
     }
 
     pub async fn set_user_private_data(&self, args: &SetUserPrivateData, conn: &mut DbConn<'_>) -> api::Result<<SetUserPrivateData as RpcTrait>::Ret> {
-        let SessionToken{user_id, version_master_key: version, ..} = self.session_token_unseal_refreshed_and_validated(&args.authed_session_token, Clearance::LoggedIn).await?;
+        let SessionToken{user_id, .. } = self.session_token_unseal_refreshed_and_validated(conn.tx().await?, &args.authed_session_token, Clearance::LoggedIn).await?;
 
         async {
-            conn.std().await?.set_user_private_data(&user_id, &args.secret_private_data).await?;
+            conn.tx().await?.set_user_private_data(&user_id, &args.secret_private_data).await?;
 
             debug!("ok");
             Ok(())
--- server/src/core/session.rs
+++ server/src/core/session.rs
@@ -1,7 +1,7 @@
 use common::{api::{self, UserId, session_token::{Clearance, SessionToken}}, crypto::crypto_boxes::AuthBox};
 use common::crypto::crypto_boxes::Auth;
 
-use crate::state::State;
+use crate::{db::sql::TxConn, state::State};
 
 impl State {
     pub fn session_token_new_need_second_factor_sealed(&self, user_id: UserId, version_master_key: u32) -> eyre::Result<AuthBox<SessionToken>> {
@@ -12,7 +12,7 @@ impl State {
         SessionToken::new_logged_in(user_id, version_master_key, auto_logout, uber).authenticate(&self.secret_key[..])
     }
 
-    pub async fn session_token_unseal_refreshed_and_validated(&self, auth_session_token: &AuthBox<SessionToken>, required_clearance: Clearance) -> api::Result<SessionToken> {
+    pub async fn session_token_unseal_refreshed_and_validated(&self, conn: &mut TxConn, auth_session_token: &AuthBox<SessionToken>, required_clearance: Clearance) -> api::Result<SessionToken> {
         let mut t = auth_session_token.get_verified(&self.secret_key[..])?;
         
         t.refresh( 
@@ -23,11 +23,9 @@ impl State {
 
         t.validate(required_clearance)?;
 
-
-        // TODO implement again
-        //if self.db.get_user_version_from_userid(&t.user_id).await? != t.version {
-        //    return Err(api::Error::InvalidSessionToken);
-        //}
+        if conn.get_user_version_master_key(&t.user_id).await? != t.version_master_key {
+            return Err(api::Error::InvalidSessionToken);
+        }
 
         Ok(t)
     }
